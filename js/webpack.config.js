f2644729e20d4bc946bcaaff1c8ae2dce70b3ea8
Aditya Wiguna <adityawiguna0@gmail.com>
2026-02-06 17:07:46 +0800

    Disable source maps to prevent JSON errors in all environments

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -2,9 +2,7 @@ const config = require('flarum-webpack-config');
 
 const webpackConfig = config();
 
-// Disable source maps to prevent JSON errors in production
-if (process.env.NODE_ENV === 'production') {
-  webpackConfig.devtool = false;
-}
+// Disable source maps to prevent JSON errors
+webpackConfig.devtool = false;
 
 module.exports = webpackConfig;
